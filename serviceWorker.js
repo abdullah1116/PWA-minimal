1794c9305ffd9cfdcbc185f3fa169951420049b7
1116 <tmp.abdullah1@gmail.com>
2021-01-18 02:03:46 +0500

    first commit

---
+++ serviceWorker.js
@@ -0,0 +1,10 @@
+const staticDevCoffee = 'dev-coffee-site-v1';
+const assets = ['/', '/index.html'];
+
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
