950a38d713b4350948f7002b412d919f94b39721
tmpabdullah <tmp.abdullah1@outlook.com>
2021-05-11 12:40:08 +0500

    update assets path

--- sw.js
+++ sw.js
@@ -1,37 +1,22 @@
 const staticCacheName = 'sw-cache';
 const assets = [
-  '/',
-  '/favicon.ico',
-  '/manifest.json',
-  '/assets/images/logo.png',
-  '/assets/images/icon/icon-192x192.png',
-  '/assets/js/index.js',
+  'index.html',
+  'favicon.ico',
+  'manifest.json',
+  'assets/images/logo.png',
+  'assets/images/icon/icon-192x192.png',
+  'assets/js/index.js',
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('sw: installed', evt);
-
-  evt.waitUntil(self.skipWaiting());
-  return;
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log('sw: caching');
       cache.addAll(assets);
+      cache.match;
     })
   );
-});
-
-self.addEventListener('message', (evt) => {
-  console.log('sw: message', evt);
-
-  // if (event.data.type === 'CACHE_URLS') {
-  //     event.waitUntil(
-  //         caches.open(KEY)
-  //             .then( (cache) => {
-  //                 return cache.addAll(event.data.payload);
-  //             })
-  //     );
-  // }
 });
 
 self.addEventListener('activate', (evt) => {
